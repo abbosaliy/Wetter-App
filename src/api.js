6ffdfd29421229f51318dc4680d6bf464196fdfb
abbosaliy <abbosbekanvarjonov8@gmail.com>
2025-03-19 20:51:03 +0100

    add api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const apiKey = "0ecff552f23a45c2bc7200708252602";
 
 export async function fetchWeather(cityName) {
-  const url = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&lang=de&days=3`;
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityName}&lang=de&days=3`;
   const response = await fetch(url);
   const result = await response.json();
   return result;
