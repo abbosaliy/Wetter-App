d5bc6714c3af6c5670bf67d7665abbbf72282603
abbosaliy <abbosbekanvarjonov8@gmail.com>
2025-03-23 21:12:12 +0100

    remove public file condition JS

--- src/condition.js
+++ src/condition.js
@@ -4,7 +4,7 @@ export function getConditionImagePath(code, isNight = false) {
   if (!condition) return null;
 
   return (
-    "/Wetter-App/public/conditionImage/" + condition[isNight ? "night" : "day"]
+    "/Wetter-App/conditionImage/" + condition[isNight ? "night" : "day"]
   );
 }
 
